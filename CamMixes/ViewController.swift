31069542bcc0c63dfa0e4e392672bcf154210d86
hobostation <pokie@talk21.com>
2016-03-02 18:17:12 +0000

    end 02/16
    end of day

--- CamMixes/ViewController.swift
+++ CamMixes/ViewController.swift
@@ -24,6 +24,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
 
 
 }
