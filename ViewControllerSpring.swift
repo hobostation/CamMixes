a8894a135a3b646a113c2421e8c6a0929b7c03fe
hobostation <pokie@talk21.com>
2016-03-08 12:58:44 +0000

    new view controller for spring screen

---
+++ ViewControllerSpring.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerSpring.swift
+//  CamMixes
+//
+//  Created by Robbie James on 08/03/2016.
+//  Copyright © 2016 Hobostation. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSpring: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
